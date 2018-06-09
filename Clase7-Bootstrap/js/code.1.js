cf4e77a2f30b5a85334cc779acf413b18917951a
demian44 <demiandes@gmail.com>
2018-06-08 22:05:08 -0300

    subo toda la clase 7

---
+++ Clase7-Bootstrap/js/code.1.js
@@ -0,0 +1,3 @@
+function pepe(){
+    return 2;
+}
---
+++ Clase7-Bootstrap/js/code.js
@@ -0,0 +1,45 @@
+
+var miFilter = function () {
+
+    var array = [
+        { nombre: "pepe", nota: 2 },
+        { nombre: "pepe", nota: 7 },
+        { nombre: "pepe", nota: 8 }];
+
+    array.filter(function (num) {
+        if (num.nota > 2)
+            var coso = num;
+        console.log(coso);
+    })
+}
+
+
+
+var miMap = function () {
+
+    var array = [
+        { nombre: "pepe", nota: 2 },
+        { nombre: "pepe", nota: 7 },
+        { nombre: "pepe", nota: 8 }];
+
+    array.map(function (num) {
+        num.nota *= num.nota;
+        var coso = num;
+        console.log(coso);
+    });
+}
+
+
+var miReduce = function () {
+
+    var array = [
+        { nombre: "pepe", nota: 2 },
+        { nombre: "pepe", nota: 7 },
+        { nombre: "pepe", nota: 8 }];
+
+    var coso = array.reduce(function (total, num) {
+        return total += num.nota;
+    });
+
+    console.log(coso);
+}
